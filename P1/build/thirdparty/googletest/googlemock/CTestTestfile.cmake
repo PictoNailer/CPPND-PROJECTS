4b177211fbd271e04ec889858e4d92e7d64c18f9
pictonailer <GA.LTY-1997@HOTMAIL.COM>
2021-05-04 20:39:43 +0200

    Finalize Project #1

---
+++ P1/build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /root/Desktop/CPPND/P1/thirdparty/googletest/googlemock
+# Build directory: /root/Desktop/CPPND/P1/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
